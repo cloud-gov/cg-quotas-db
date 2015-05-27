e5d7a01794d7a5fc07bd5facd7dc28b70aebad8a
ramirezg <gabriel.ramirez@gsa.gov>
2015-05-27 11:51:32 -0400

    removing unused routes

--- quotas.py
+++ quotas.py
@@ -26,25 +26,6 @@ def all():
     return render_template('index.html', quotas=Quota.list_all())
 
 
-@app.route("/quotas/<guid>/", methods=['GET'])
-def one(guid):
-    data = Quota.list_one_aggregate(guid=guid)
-    if data:
-        return render_template('details.html', quota=data)
-    else:
-        return '404.html', 404
-
-
-@app.route("/quotas/<guid>/<start_date>/<end_date>", methods=['GET'])
-def dates(guid, start_date, end_date):
-    data = Quota.list_one_aggregate(
-        guid=guid, start_date=start_date, end_date=end_date)
-    if data:
-        return render_template('details.html', quota=data)
-    else:
-        return '404.html', 404
-
-
 @app.route("/api/", methods=['GET'])
 def api_index():
     """ Provide documentation for API """
