671cd60756c5d28e7316774940e65c2608cab1e6
Jeremia Kimelman <jbkimelman@gmail.com>
2015-06-17 19:39:20 -0400

    detailview

--- static/js/app/views/DetailView.js
+++ static/js/app/views/DetailView.js
@@ -7,14 +7,12 @@ var QuotaModel = require('../models/QuotaModel');
 var templateHtml = fs.readFileSync(__dirname + '/../templates/DetailView.html').toString();
 
 var DetailView = Backbone.View.extend({
-  tagName: 'div',
   model: QuotaModel,
   template: _.template(templateHtml),
   initialize: function initialize () {
     this.render();
   },
   render: function render () {
-    console.log('rendering');
     var html = this.template(this.model.toJSON());
     this.$el.html(html);
   }
