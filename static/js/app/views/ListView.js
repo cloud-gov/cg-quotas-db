e1b3a27991bdffab0cbdfbef28591c081da3fc81
Jeremia Kimelman <jbkimelman@gmail.com>
2015-06-18 16:23:02 -0400

    list view form handlers

--- static/js/app/views/ListView.js
+++ static/js/app/views/ListView.js
@@ -10,12 +10,21 @@ var formTemplate = _.template(formControlHtml);
 var listTemplateHtml = fs.readFileSync(__dirname + '/../templates/ListView.html').toString();
 
 var ListView = Backbone.View.extend({
+  events: {
+    'click #filter-action': 'filter',
+    'click #reset-action': 'clearFilter',
+    'click #download-csv-action': 'downloadCSV'
+  },
   initialize: function initialize () {
     this.listenTo(this.collection, 'sync', this.render);
+    this.listenTo(this.collection, 'update', function (x){
+      console.log('x', x);
+    });
   },
   render: function render () {
+    console.log('rendering');
     this.$el.html(listTemplateHtml);
-    var formControls = this.$('formset');
+    var formControls = this.$('form');
     var tbody = this.$('tbody');
 
     var formHtml = formTemplate({ collection: this.collection.toJSON() });
@@ -25,6 +34,22 @@ var ListView = Backbone.View.extend({
        var quota = new ListItemView({ model: model });
        tbody.append(quota.$el);
      }, this);
+  },
+  filter: function filter (e) {
+    e.preventDefault();
+    var formData = this.$('form').serializeArray();
+    var filterData = {};
+    _.each(formData, function(field) {
+      if (field.value != '') { filterData[field.name] = field.value; }
+    });
+    this.collection.filterByDates(filterData['since-date'], filterData['until-date'], filterData['quota-selection']);
+  },
+  clearFilter: function clearFilter (e) {
+    e.preventDefault();
+    this.$('form')[0].reset();
+  },
+  downloadCSV: function downloadCSV (e) {
+    console.log('k, downloading.... (not really)');
   }
 });
 
