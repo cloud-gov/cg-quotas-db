d06d3857bfe2c445c67dc70e9f292ebcf602346b
Jeremia Kimelman <jbkimelman@gmail.com>
2015-06-18 16:22:51 -0400

    bye bye detail view; hello filters

--- static/js/app/models/QuotaCollection.js
+++ static/js/app/models/QuotaCollection.js
@@ -10,7 +10,7 @@ var QuotaCollection = Backbone.Collection.extend({
   initialize: function initialize () {
     this.fetch();
   },
-  search: function search (id) {
+  getId: function search (id) {
     var model = this.get(id);
     if (model) {
       return $.Deferred().resolveWith(this, model);
@@ -19,6 +19,22 @@ var QuotaCollection = Backbone.Collection.extend({
       model = new QuotaModel({ guid: id });
       return model.fetch();
     }
+  },
+  filterByDates: function filterByDates (sinceDate, untilDate, guid) {
+    console.log('filtering by dates');
+    console.log('sinceDate', sinceDate);
+    console.log('untilDate', untilDate);
+    console.log('guid', guid);
+    if (sinceDate === undefined && untilDate === undefined && guid === undefined) {
+      return this;
+    }
+    var dates = {
+      'since': sinceDate,
+      'until': untilDate
+    };
+    this.trigger('sync');
+
+    return this.fetch({data: dates});
   },
   parse: function parse (response) {
     return response.Quotas;
--- static/js/app/router.js
+++ static/js/app/router.js
@@ -2,9 +2,6 @@ var Backbone = require('backbone');
 var $ = require('jquery');
 
 var ListView = require('./views/ListView');
-var DetailView = require('./views/DetailView');
-
-var QuotaModel = require('./models/QuotaModel');
 
 var mainView = $('main').append('div').addClass('app');
 
@@ -20,24 +17,6 @@ var Router = Backbone.Router.extend({
     listView = app.viewCache['listView'] || new ListView({ collection: this.quotas });
     app.viewCache['listView'] = listView;
     mainView.html(listView.$el);
-  },
-  showQuota: function showQuota (guid) {
-    var quotas = this.quotas;
-    var model = quotas.get(guid);
-    if (model) {
-      var detailView = new DetailView({model: model});
-      mainView.html(detailView.$el);
-    }
-    else {
-      var newModel = new QuotaModel({ guid: guid });
-      newModel.fetch({
-        success: function () {
-          console.log('new model', newModel);
-          var detailView = new DetailView({model: newModel});
-          mainView.html(detailView.$el);
-        }
-      })
-    }
   }
 });
 
