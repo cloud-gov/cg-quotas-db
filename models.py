b19660d9b33fc500de868e05fa99d70ae460e28d
ramirezg <gabriel.ramirez@gsa.gov>
2015-11-02 14:36:56 -0500

    removing services from models.py

--- models.py
+++ models.py
@@ -3,38 +3,6 @@ from quotas import db
 from sqlalchemy.orm import relationship
 
 
-class Service(db.Model):
-    """ Model for storing specific service data """
-
-    __tablename__ = 'service'
-
-    quota = db.Column(db.String, db.ForeignKey('quota.guid'))
-    guid = db.Column(db.String())
-    date_collected = db.Column(db.Date())
-    instance_name = db.Column(db.String())
-    label = db.Column(db.String())
-    provider = db.Column(db.String())
-
-    # Limiting the data by date
-    __table_args__ = (db.PrimaryKeyConstraint(
-        'quota', 'guid', 'date_collected', name='quota_serviceguid_date'),)
-
-    def __init__(self, quota, guid, instance_name,
-                 label=None, provider=None, date_collected=None):
-        self.quota = quota
-        self.guid = guid
-        self.instance_name = instance_name
-        self.label = label
-        self.provider = provider
-        if date_collected:
-            self.date_collected = date_collected
-        else:
-            self.date_collected = date.today()
-
-    def __repr__(self):
-        return '<guid {0} date {1}>'.format(self.quota, self.date_collected)
-
-
 class QuotaData(db.Model):
     """ Model for a specific Quotas's data """
 
@@ -72,7 +40,6 @@ class Quota(db.Model):
     created_at = db.Column(db.DateTime())
     updated_at = db.Column(db.DateTime())
     data = relationship("QuotaData")
-    services = relationship("Service")
 
     def __init__(self, guid, name=None, url=None):
         self.guid = guid
